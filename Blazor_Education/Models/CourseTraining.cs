7db9ec2e146853bd5eb24e391f36381f08229996
Umut <kayabay.umut@gmail.com>
2023-12-19 17:43:25 +0300

    My project created

---
+++ Blazor_Education/Models/CourseTraining.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor_Education.Models
+{
+    public class CourseTraining
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Blazor_Education/Models/DropDown.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor_Education.Models
+{
+    public class DropDown
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
